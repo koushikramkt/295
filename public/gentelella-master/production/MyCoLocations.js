98f35772a696025ddbaa821cca92e824f10adef1
Jeyanthh <jeyanthhvr@gmail.com>
2017-04-21 18:09:41 -0700

    oil train page

---
+++ public/gentelella-master/production/MyCoLocations.js
@@ -0,0 +1,90 @@
+eqfeed_callback({
+  "locations":
+  {
+    "location":
+    [
+      {
+        "id": "sfo",
+        "type": "RailwayStation",
+        "latitude":37.77397,
+        "longitude":-122.431297
+      },
+    
+      {
+        "id": " south san francisco",
+        "type": "RailwayStation",
+        "latitude": 37.6547,
+        "longitude":-122.4077,
+      },
+      {
+      "id": "Milbrae",
+      "type": "RailwayStation",
+      "latitude": 37.6002,
+      "longitude":-122.3869
+      },
+      {
+        "id": "Burlingame",
+        "type": "RailwayStation",
+        "latitude": 37.577,
+        "longitude": -122.3481
+      },
+      {
+        "id": "Hillsdale",
+        "type": "RailwayStation",
+        "latitude": 37.537,
+        "longitude": -122.3004
+      },
+      {
+        "id": "San Carlos",
+        "type": "RailwayStation",
+        "latitude": 37.5072,
+        "longitude":  -122.2605
+      },
+      {
+        "id": "Menlo Park",
+        "type": "RetailLocation",
+        "latitude": 37.4530,
+        "longitude": -122.1817
+      },
+      {
+        "id": "San Antonio",
+        "type": "RailwayStation",
+        "latitude": 37.4419,
+        "longitude": -122.1930
+      },
+      {
+        "id": "Santa CLara",
+        "type": "RailwayStation",
+        "latitude": 37.354,
+        "longitude": -121.9552
+      }, {
+      "id": "San Jose Diridon",
+      "type": "RailwayStation",
+      "latitude": 37.329,
+      "longitude":  -121.9025
+      },{
+      "id": "Carson",
+      "type": "RailwayStation",
+      "latitude": 37.279518,
+      "longitude": -121.867905
+      },
+      {
+        "id": "blosson hill",
+        "type": "RailwayStation",
+        "latitude": 37.2524,
+        "longitude": -121.7975
+      },
+      {
+        "id": "Hillsdale",
+        "type": "RailwayStation",
+        "latitude": 37.1305012,
+        "longitude": -121.6543901
+      },
+      {
+        "id": "Gilroy",
+        "type": "RailwayStation",
+        "latitude": 37.0057,
+        "longitude": -121.5682      }
+    ]
+  }
+});
