51f9f7237dc5e8de167a9ae9039eefa68610e67a
sindhuja <narrasindhujah@gmail.com>
2017-04-22 00:41:10 -0700

    updated maps in all 4 scenarios

---
+++ public/gentelella-master/production/mapdata.js
@@ -0,0 +1,130 @@
+mapdata = {
+
+		maplocations:
+		[
+			{
+				"name":"San Jose Hospital",
+				"latitude": 37.3432744,
+				"longitude": -121.8768993,
+				"type": "Hospital"
+				
+			},		
+			{
+				"name": "Agnews State Hospital (historical)",
+				"latitude": 37.3941071,
+				"longitude": -121.9510693,
+				"type": "Hospital"
+
+			},
+			{
+				"name": "Columbia Mission Oaks Hospital",
+				"latitude": 37.2441667,
+				"longitude": -121.9483333,
+				"type": "Hospital"
+
+			},		
+			{
+				"name": "Good Samaritan Hospital, CA",
+				"latitude" : 37.2518881,
+				"longitude" : -121.9463451,
+				"type": "Hospital"
+
+			},		
+			{
+
+				"name": "Henard Hospital, CA",
+				"latitude": 37.2268887,
+				"longitude": -121.969124,
+				"type": "Hospital"
+			},		
+			{
+
+				"name": "Kaiser Hospital, CA",
+				"latitude" : 37.3410529,
+				"longitude": -121.9785696,
+				"type": "Hospital"
+
+			},
+			{
+				"name": "Los Gatos Community Hospital, CA",
+				"latitude": 37.2627213,
+				"longitude": -121.9696794,
+				"type": "Hospital"
+
+			},		
+			{
+
+				"name": "Lucile Packard Childrens Hospital",
+				"latitude": 37.4354946,
+				"longitude": -122.1749647,
+				"type": "Hospital"
+
+			},
+			
+			{		
+				"name": "Lion Food Center",
+				"latitude":37.2968505,
+				"longitude":-121.8174135,
+				"type": "Food"
+			},
+			{
+				"name":"Arteaga's Food Center",
+				"latitude":37.2968505,
+				"longitude":-121.8174135,
+				"type":"Food"
+			},
+			{
+				"name":"Coto's Mother Baby Food Center",
+				"latitude":37.2968505,
+				"longitude":-121.8174135,
+				"type":"Food"
+			},
+			{
+				"name":"La Hacienda Food Center",
+				"latitude":37.4447334,
+				"longitude":-122.1195375,
+				"type":"Food"
+			},
+			{
+				"name": "Mi Pueblo Food Center",
+				"latitude": 37.4447334,
+				"longitude": -122.1195375,
+				"type": "Food"
+			},
+			{
+				"name": "Merrill Gardens at Gilroy",
+				"latitude": 37.5184128,
+				"longitude": -121.9346928,
+				"type": "Communities"
+			},
+			{
+				"name": "Crosswalk Community Church",
+				"latitude": 37.5184128,
+				"longitude": -121.9346928,
+				"type": "Communities"
+			},
+			{
+				"name": "Sacred Heart Community Service",
+				"latitude": 37.5157647,
+				"longitude": -121.934702,
+				"type": "Communities"
+			},
+			{
+				"name": "Sunnyvale Community Resources",
+				"latitude": 37.5157647,
+				"longitude": -121.934702,
+				"type": "Communities"
+			},
+			{
+				"name": "Black Community Services Center",
+				"latitude": 37.5157647,
+				"longitude": -121.934702,
+				"type": "Communities"
+			}
+			
+			
+		]
+	
+
+
+}
